a732bae44fb1fac3a5a1b558bdcf8234074f8073
Kishaunjith S <kishaunjithsenthiljith@gmail.com>
2026-03-01 11:11:43 +0530

    Stable version: Secure chat + steganography working - Without Image over chat

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyA37ooQjlOpA8ZgJHdM6lNxyZv-1IXHw7c",
+    authDomain: "chat-17acb.firebaseapp.com",
+    projectId: "chat-17acb",
+    storageBucket: "chat-17acb.firebasestorage.app",
+    messagingSenderId: "888456428584",
+    appId: "1:888456428584:web:33f9e3f3a18ba6d4c2c91d"
+  },
+  production: true
+};
